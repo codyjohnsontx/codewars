5ae58ea75171716d35b6c74e2b92ce30ff65d140
Cody Johnson <codyjohnsontx@gmail.com>
2023-12-19 21:33:34 -0600

    completed another coding challenege

---
+++ 8kyu/remove-string-spaces.py
@@ -0,0 +1,6 @@
+#Write a function that removes the spaces from the string, then return the resultant string.
+# Example "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+#My solution
+def no_space(x):
+    return x.replace(" ","")
