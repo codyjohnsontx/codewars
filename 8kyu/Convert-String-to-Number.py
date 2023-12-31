c1791d004e913044ff18d9f74987eee526a233d9
Cody Johnson <codyjohnsontx@gmail.com>
2023-12-31 10:06:08 -0600

    add convert string to number python challenge

---
+++ 8kyu/Convert-String-to-Number.py
@@ -0,0 +1,12 @@
+# Description
+# We need a function that can transform a string into a number. 
+# What ways of achieving this do you know?
+
+# Examples
+# "1234" --> 1234
+
+
+#My Solution
+
+def string_to_number(s):
+    return int(s)
