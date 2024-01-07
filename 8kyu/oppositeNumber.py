88d3bf62440dab48b6b636340d6a3f68e1657230
Cody Johnson <codyjohnsontx@gmail.com>
2024-01-07 15:03:29 -0600

    add oppositeNumber.py to challenges

---
+++ 8kyu/oppositeNumber.py
@@ -0,0 +1,16 @@
+# Very simple, given an integer or a 
+# floating-point number, find its opposite.
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34
+
+# My Solution
+def opposite(number):
+    return -1 * number
+
+
+
+# Multiplying a number by -1 will always return its opposite
