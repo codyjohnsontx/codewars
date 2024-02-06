925fa7f1331b89a7b1ba33ca8c116f1150436ada
Cody Johnson <codyjohnsontx@gmail.com>
2024-02-06 16:46:54 -0600

    add 8kyu challenge

---
+++ 8kyu/youCantCodeUnderPressure.js
@@ -0,0 +1,12 @@
+// Code as fast as you can! You need to
+// double the integer and return it.
+
+// function doubleInteger(i) {
+// return i;
+//   }
+
+// My solution
+function doubleInteger(i) {
+  i = i * 2;
+  return i;
+}
