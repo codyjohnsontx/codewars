c7438adfa170c439b119e1e915d3a055bd893fa4
Cody Johnson <codyjohnsontx@gmail.com>
2024-02-29 17:59:16 -0600

    solution was incomplete, add if else statement to avoid error happening when empty array return 0

--- 8kyu/calAverage.js
+++ 8kyu/calAverage.js
@@ -6,6 +6,10 @@
 // My solution
 
 function findAverage(array) {
-  const sum = array.reduce((a, b) => a + b, 0);
-  return sum / array.length;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    const sum = array.reduce((a, b) => a + b, 0);
+    return sum / array.length;
+  }
 }
