d712a99ac3b5f32bbd3929ba7bf23c96a9417da8
Cody Johnson <codyjohnsontx@gmail.com>
2024-07-04 14:11:45 -0500

    add getCharFromASCIIValues.js to 8kyu challenges

---
+++ 8kyu/getCharfromASCIIValues.js
@@ -0,0 +1,16 @@
+// Write a function which takes a number and returns the
+// corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0'
+
+// My solution
+// Had to search how to convert
+// Use String.fromCharCode() method
+
+function getChar(char) {
+  return String.fromCharCode(char);
+}
