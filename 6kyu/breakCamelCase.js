742464f494fd0e270bee7fa1059ac131eb84e0e7
Cody Johnson <codyjohnsontx@gmail.com>
2024-08-17 14:02:53 -0500

    add breakCamelCase.js to 6kyu challenges

--- 6kyu/breakCamelCase.js
+++ 6kyu/breakCamelCase.js
@@ -7,4 +7,31 @@
 // ""             =>  ""
 
 
-// Params
+// P
+// always be letters, no numbers
+// no spaces between letters
+// no capitals in first Letter, will always be camelCase or empty string
+// 
+// R
+// how do i return? return or console.log?
+// return
+// 
+// E
+// function camelCase(word){
+
+// }
+// camelCase(javaScript) => java Script
+// camelCase(toYota) => to Yota
+// camelCase(lexus) => lexus
+// 
+// P
+function camelCase(word){
+    // break word apart into an array
+    // use map function to create new array to call a provided function on
+    // if letter in positon (i) equals that letter but capitalized return it to new array
+    // else return just the letter
+    // once completed join the array back into word
+    return word.split('').map(char => {
+        return char === char.toUpperCase() ? " " + char : char
+    }).join('')
+}
