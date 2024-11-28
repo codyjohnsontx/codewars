5e9b058a621c1545b56dac0d220b6ccd79cda4fa
Cody Johnson <codyjohnsontx@gmail.com>
2024-11-28 13:51:57 -0600

    update flattenAndSortAnArray.js

--- 7kyu/flattenAndSortAnArray.js
+++ 7kyu/flattenAndSortAnArray.js
@@ -20,6 +20,10 @@
 
 'use strict';
 
+let array = [[3, 2, 1], [4, 6, 5], [100], [9, 7, 8]];
+
 function flattenAndSort(array) {
   return array.flat().sort((a, b) => a - b);
 }
+
+// output return -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
