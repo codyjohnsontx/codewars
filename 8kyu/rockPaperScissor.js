900e63530c9b987573be0c615f4dfdc6db9ce27f
Cody Johnson <codyjohnsontx@gmail.com>
2025-06-01 15:31:46 -0500

    add rockPaperScissor.js to 8kyu challenges

---
+++ 8kyu/rockPaperScissor.js
@@ -0,0 +1,29 @@
+// Rules of the "Rock, Paper, Scissors" game are:
+
+// Rock beats Scissors,
+// Scissors beat Paper,
+// Paper beats Rock,
+// Two identical moves are a draw.
+// Let's play! You will be given valid moves of two Rock, Paper, Scissors players, and have to return which player won: "Player 1 won!" for player 1, and "Player 2 won!" for player 2. In case of a draw return Draw!.
+
+// Examples:
+// "scissors",     "paper"     --> "Player 1 won!"
+// "scissors",     "rock"      --> "Player 2 won!"
+// "paper",        "paper"     --> "Draw!"
+
+// Solution
+function rps(player1, player2) {
+  if (player1 === player2) {
+    return 'Draw!';
+  }
+
+  if (
+    (player1 === 'rock' && player2 === 'scissors') ||
+    (player1 === 'scissors' && player2 === 'paper') ||
+    (player1 === 'paper' && player2 === 'rock')
+  ) {
+    return 'Player 1 won!';
+  }
+
+  return 'Player 2 won!';
+}
